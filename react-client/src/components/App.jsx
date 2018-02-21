7fe7c7d48f5ab29522ca051568a6fc34d35f634a
Jacob Granberry <jacobgranberry@gmail.com>
2018-02-21 11:11:06 -0600

    Fixes styling for loginform

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import LoginForm from './Login.jsx';
 
 const App = ({ store }) => (
-  <Provider store={store}>
-    <h1>Grassroots</h1>
-  </Provider>
+  <div>
+    <LoginForm/>
+      <Provider store={store}>
+        <h1>Grassroots</h1>
+      </Provider>
+  </div>
 )
 
 export default App;
--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+
 
 const LoginForm = () =>  (
     <div className = 'login-form'>
-  
-  
+     <style>{`
+      body > div,
+      body > div > div,
+      body > div > div > div.login-form {
+        height: 100%;
+      }
+    `}</style>
       <Grid
         textAlign='center'
-        
+        style = {{ height: '100%' }}
         verticalAlign='middle'
       >
-        <Grid.Column >
+        <Grid.Column style={{ maxWidth: 450 }}>
           <Header as='h2' textAlign='center'>
             {' '}Log-in to your account
           </Header>
