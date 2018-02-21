3e630d80a34251942a4f8e94a375551d4f187543
Sean Quillen <sean.r.quillen@gmail.com>
2018-02-21 10:25:15 -0600

    stubs out server requirements

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+let express = require('express');
+let bodyParser = require('body-parser');
+let db = require('../database-mysql');
+let session = require('express-session');
+require('dotenv').config();
+// let requestHelpers = require('../helpers.js') // to implement from Shannon
+// let serverHelpers = require('../lib/serverHelpers.js') // 
 
-var items = require('../database-mysql');
 
-
-var app = express();
-
+let app = express();
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(session({
+  secret: process.env.SESSION_PASSWORD,
+  resave: false,
+  saveUninitialized: true
+}));
+
+//MAIN PAGE REQUESTS
+app.get('/', isLoggedIn,function(req, res) {
+  
+})
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
