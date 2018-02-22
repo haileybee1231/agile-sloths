c82179ad3f0293753947cfa439e143e6bc9dcfcd
Jacob Granberry <jacobgranberry@gmail.com>
2018-02-22 12:20:01 -0600

    Stubs out Title of sidebar

--- react-client/src/components/Sidebar.jsx
+++ react-client/src/components/Sidebar.jsx
@@ -1,1 +1,17 @@
 import React from 'react'
+import { Menu } from 'semantic-ui-react'
+
+class Sidebar extends React.Component {
+
+    render() {
+        return (
+            <Menu vertical>
+                <Menu.Item>
+                    <Menu.Header>Test</Menu.Header>
+                </Menu.Item>
+            </Menu>
+        )
+    }
+}
+
+export default Sidebar
