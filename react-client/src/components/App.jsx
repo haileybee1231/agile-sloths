d004fe88a167ede82a1e291fa9f4230cec9c533c
haileybee1231 <haileybee1231@gmail.com>
2018-02-22 11:41:08 -0600

    Refactor to Redux

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import LoginForm from './Login.jsx';
 
-const App = ({ store }) => (
-  <div>
-      <Provider store={store}>
-        <LoginForm/>
-      </Provider>
-  </div>
-)
+const App = ({ store }) => { // pass store to app
+  return (
+    <Provider store={store}>
+      <div>
+          <LoginForm />
+        </div>
+    </Provider>
+  )
+}
 
 export default App;
--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -1,52 +1,66 @@
-import React from 'react'
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import React from 'react';
+import login from '../../src/actions/actions.js'; // import action
+import { connect } from 'react-redux'; // used to connect "smart" components with actions
+import { bindActionCreators } from 'redux'; // allows you to bind actions to methods
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
 
+const LoginForm = (props) =>  ( //pass in props
+  <div className = 'login-form'>
+   <style>{`
+    body > div,
+    body > div > div,
+    body > div > div > div.login-form {
+      height: 100%;
+    }
+  `}</style>
+    <Grid
+      textAlign='center'
+      style = {{ height: '100%' }}
+      verticalAlign='middle'
+    >
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header size='huge' style={{ fontSize: 60 }}>GRASSROOTS</Header>
+        <Header as='h2' color='green' textAlign='center'>
+          {' '}Log-in to your account
+        </Header>
 
-const LoginForm = () =>  (
-    <div className = 'login-form'>
-     <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
-      <Grid
-        textAlign='center'
-        style = {{ height: '100%' }}
-        verticalAlign='middle'
-      >
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header size='huge' style={{ fontSize: 60 }}>GRASSROOTS</Header>
-          <Header as='h2' color='green' textAlign='center'>
-            {' '}Log-in to your account
-          </Header>
-  
-          <Form size='large'>
-            <Segment stacked>
-              <Form.Input
-                fluid
-                icon='user'
-                iconPosition='left'
-                placeholder='Username'
-              />
-              <Form.Input 
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Password'
-                type='password'
-              />
-                
-              <Button color='green' fluid size='large'>Login</Button>
-            </Segment>
-          </Form>
-          <Message>
-            New to Grassroots?  <a href='#'>Sign up</a>
-          </Message>
-        </Grid.Column>
-      </Grid>
-    </div>
-  )
+        <Form size='large'>
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon='user'
+              iconPosition='left'
+              placeholder='Username'
+            />
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+              type='password'
+            />
 
-  export default LoginForm
+            <Button
+              onClick={() => {
+                props.login('test', 'test');  {/* call action which is on props */}
+              }}
+              color='green'
+              fluid size='large'
+            >
+            Login
+          </Button>
+          </Segment>
+        </Form>
+        <Message>
+          New to Grassroots?  <a href='#'>Sign up</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  </div>
+)
+
+const mapDispatchToProps = (dispatch) => { // takes dispatch method from store
+  return bindActionCreators({login}, dispatch); // attaches dispatch to login action so that
+}                                       //  when it fires, it dispatches an action
+
+export default connect(null, mapDispatchToProps)(LoginForm); // how you connect a "smart" component to props
