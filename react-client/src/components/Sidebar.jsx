43e10743f7ffced09d580aa6a61252d01a7070d3
Jacob Granberry <jacobgranberry@gmail.com>
2018-02-22 17:45:50 -0600

    Makes Sidebar scrollable and fixes it to left side of page

--- react-client/src/components/Sidebar.jsx
+++ react-client/src/components/Sidebar.jsx
@@ -1,25 +1,45 @@
 import React from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Input, Header } from 'semantic-ui-react'
 
 class Sidebar extends React.Component {
 
+    handleItemClick(name){ this.setState({ activeItem: name }) }
+
     render() {
+        const { activeItem } = this.state || {}
         return (
-            <Menu vertical>
+            <Menu vertical fixed = 'left' style={{overflowY: 'scroll'}} size = 'large'>
+            <Menu.Item>
+                <Header as='h2' textAlign='center' size='huge'>GRASSROOTS</Header>
+            </Menu.Item>
+            <Menu.Item>
+                <Input className='icon' icon='search' placeholder='Search...' />
+            </Menu.Item>
                 <Menu.Item>
                     <Menu.Header>Elections</Menu.Header>
-
                     <Menu.Menu>
-                        <Menu.Item name='Governor'/>
-                        <Menu.Item name='Dog Catcher'/>
-                        <Menu.Item name='President'/>
-                        <Menu.Item name='Senator'/>
-                        <Menu.Item name='School Board'/>
-                        <Menu.Item name='District Representative'/>
-                        <Menu.Item name='House Member'/>
+                        {/* map out elections here */}
+                        <Menu.Item name='governor' active={activeItem === 'governor'} onClick={this.handleItemClick}/>
+                        <Menu.Item name='Dog Catcher' active={activeItem === 'dog catcher'} onClick={this.handleItemClick}/>
+                        <Menu.Item name='President' active={activeItem === 'president'} onClick={this.handleItemClick}/>
+                        <Menu.Item name='Senator' active={activeItem === 'senator'} onClick={this.handleItemClick}/>
+                        <Menu.Item name='School Board' active={activeItem === 'school board'} onClick={this.handleItemClick}/>
+                        <Menu.Item name='District Representative' active={activeItem === 'district representative'} onClick={this.handleItemClick}/>
+                        <Menu.Item name='House Member' active={activeItem === 'house member'} onClick={this.handleItemClick}/>
+                    </Menu.Menu>
+                </Menu.Item>
+
+                <Menu.Item>
+                    <Menu.Header>Candidates</Menu.Header>
+                    <Menu.Menu>
+                        {/* map out candidates here */}
+                        <Menu.Item name='beto oRourke' active={activeItem === 'beto oRourke'} onClick={this.handleItemClick}/>
+
+                        
                     </Menu.Menu>
                 </Menu.Item>
             </Menu>
+            
         )
     }
 }
