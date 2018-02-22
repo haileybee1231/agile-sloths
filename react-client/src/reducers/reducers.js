72fd90d82c663cbb7fd5b3fe0ae688677324ed55
haileybee1231 <haileybee1231@gmail.com>
2018-02-22 15:43:56 -0600

    Change reducer name

--- react-client/src/reducers/reducers.js
+++ react-client/src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import data from '../../src/testdata.js';
 
-const testReducer = (state = data, action) => { // reducers are dispatched here
+const mainReducer = (state = data, action) => { // reducers are dispatched here
   console.log(state, action); // this log is just to prove that state is in fact changing, it will be removed soon
   switch(action.type) { // if their action type matches a case,
     case 'LOGIN':
@@ -9,4 +9,4 @@ const testReducer = (state = data, action) => { // reducers are dispatched here
   }
 }
 
-export default testReducer;
+export default mainReducer;
--- react-client/src/store/index.js
+++ react-client/src/store/index.js
@@ -1,9 +1,9 @@
 import { createStore } from 'redux';
 import { combineReducers } from 'redux';
-import testReducer from '../../src/reducers/testReducer.js';
+import mainReducer from '../../src/reducers/reducers.js';
 
 const reducer = combineReducers({ // combine all reducers from reducer file
-  data: testReducer
+  data: mainReducer
 });
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); // create store and allow for chrome redux plugin
