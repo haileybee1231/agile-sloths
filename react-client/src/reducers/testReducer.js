35103a2d3459f9916d00a3c65ecda7cf3a0b7edf
haileybee1231 <haileybee1231@gmail.com>
2018-02-21 18:41:49 -0600

    incorporates changes

--- react-client/src/reducers/testReducer.js
+++ react-client/src/reducers/testReducer.js
@@ -1,11 +1,8 @@
 import data from '../../src/testdata.js';
 
-const testReducer = (state = data, action) => { // reducers are dispatched here
-  switch(action.type) { // if their action type matches a case,
-    case 'LOGIN':
-      return Object.assign({}, state, {user: action.payload}) // they do something to state
-    default: return state;
-  }
+const testReducer = (state = data, action) => {
+  console.log(state, action);
+  return state;
 }
 
 export default testReducer;
--- react-client/src/store/index.js
+++ react-client/src/store/index.js
@@ -2,10 +2,10 @@ import { createStore } from 'redux';
 import { combineReducers } from 'redux';
 import testReducer from '../../src/reducers/testReducer.js';
 
-const reducer = combineReducers({ // combine all reducers from reducer file
+const reducer = combineReducers({
   data: testReducer
 });
 
-const store = createStore(reducer); // create store
+const store = createStore(reducer);
 
 export default store;
