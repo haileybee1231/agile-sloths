05e96b45a87d1b927131744aa3cd9313459aece2
Jacob Granberry <jacobgranberry@gmail.com>
2018-02-26 07:36:31 -0600

    Completes Feed component

--- react-client/src/components/Feed.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const FeedList = () => (
-    <div>
-        
-    </div>
-)
-
-export default FeedList
--- react-client/src/components/FeedList.jsx
+++ react-client/src/components/FeedList.jsx
@@ -2,33 +2,38 @@ import React from 'react';
 import { Grid, Container, Header, Segment, Divider, Feed } from 'semantic-ui-react'
 import Sidebar from './Sidebar.jsx'
 import data from '../testdata.js'
+const uuidv4 = require('uuid/v4')
 
 const FeedList = () => (
     <div>
         <Grid container style={{paddingLeft: 230}}>
                 <Sidebar />
             <Grid.Row style={{paddingTop: 130}}>
-                <Header as='h1'  >Feed</Header>
+                <Header as='h1'>Feed</Header>
                 <Divider/>
             </Grid.Row>
             <Divider/>
             <Grid.Row >
-                <Segment>
+                
                 <Feed>
-                    <Feed.Event>
-                    <Feed.Label image='/assets/images/avatar/small/laura.jpg' />
-                    <Feed.Content>
-                        <Feed.Date>3 days ago</Feed.Date>
-                        <Feed.Summary>
-                        <a>Laura Faucet</a> created a post
-                        </Feed.Summary>
-                        <Feed.Extra text>
-                        Have you seen what's going on in Israel? Can you believe it.
-                        </Feed.Extra>
-                    </Feed.Content>
-                    </Feed.Event>
+                    {data.events.map((event) => {
+                        return (
+                            <Feed.Event key={uuidv4()}>
+                                <Feed.Label image='https://react.semantic-ui.com/assets/images/avatar/small/laura.jpg' />
+                                <Feed.Content>
+                                    <Feed.Date>3 days ago</Feed.Date>
+                                    <Feed.Summary>
+                                    <a>{event.host}</a> created an event {event.title}
+                                    </Feed.Summary>
+                                    <Feed.Extra text>
+                                    {event.description}
+                                    </Feed.Extra>
+                                </Feed.Content>
+                            </Feed.Event>
+                        )
+                    })}
                 </Feed>
-                </Segment>
+               
             </Grid.Row>
         </Grid>
         
